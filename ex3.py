e7dbd5b3a38ee70d514b76620e4ca45f7849bc9f
diy-z <t19863751130@gmail.com>
2020-08-30 23:16:37 +0800

    提交

---
+++ ex3.py
@@ -0,0 +1,12 @@
+print ("hh")
+print ("Hens",25+30/6)
+print ("Hens_extend",25+6)
+print ("float",30/6)
+print (3 + 2 < 3 + 2 + 1)
+print ("What is 3 + 2?",3 + 2)
+print ("What is 5 - 7?",5 - 7)
+
+print ("Is it greater?",5 > -2)
+print ("Is it greater or equal?",5 >= -2)
+
+print ("7 / 4与7.0 / 4.0相同吗",7 / 4 == 7.0 / 4.0)
